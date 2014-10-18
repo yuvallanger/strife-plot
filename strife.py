c83aa04f921bb23c29a9a59d0f709f8b93005de2
Yuval Langer <yuval.langer@gmail.com>
2014-10-18 11:51:16 +0300

    Adding latest stuff

--- strife.py
+++ strife.py
@@ -1,23 +1,33 @@
 #!/usr/bin/env python3
 
-from numba import jit
-from contracts import contract
+from __future__ import print_function
 
-from glob import glob
 import os
+import sys
+from glob import glob
+
+# import contracts
+# from contracts import contract
 
 import numpy as np
-import matplotlib.pyplot as plt
 import matplotlib as mpl
-import h5py
+import matplotlib.pyplot as plt
 
-#from scipy.ndimage.filters import convolve
+import h5py
+from numba import jit
+
+# from scipy.ndimage.filters import convolve
+
+# contracts.disable_all()
+
+DATA_DIR_PATH = 'data/'
 
 HDF5_SNAPSHOTS_DATA_PATH = '/DataSamples/Snapshots/Data'
 HDF5_FREQUENCIES_DATA_PATH = '/DataSamples/Frequencies/Data'
 
-COLORS_GRID = ((
-    # http://blog.mollietaylor.com/2012/10/color-blindness-and-palette-choice.html
+# http://blog.mollietaylor.com/2012/10/color-blindness-and-palette-choice.html
+COLORS_GRID = (
+    (
         ('#0875C2'),
         ('#6BA8D4'),
         ('#3288C4'),
@@ -56,45 +66,43 @@ STRAINS_MAPPING = {
     7: dict(strain='S2R2', color=COLORS_GRID[3][0]),
 }
 
+
+#@contract(data_dir_path='str', returns='dict(str:(list[10](list[10])))')
 def get_hdf5_grid():
-    '''Returns a dict()'''
-    @contract(filename_template='str', returns='list[10](list[10](str))')
-    def get_filename_list(filename_template):
-        return [[
-            glob(filename_template.format(
-                sth=row_i,
-                cth=col_i,
-            ))[0]
-        for col_i in range(10)]
-        for row_i in range(10)]
+    """Return a dict(str:list[10](list[10])."""
 
-    def open_h5py_files(filename_list_list):
-        return [[h5py.File(filename)
-                 for filename in filename_list]
-                 for filename_list in filename_list_list]
+    return {
+        sim_series: [
+            [
+                h5py.File(glob(
+                    DATA_DIR_PATH + filename_template.format(
+                        sth=row_i,
+                        cth=col_i,
+                    ))[0]
+                )
+                for col_i in range(10)
+            ]
+            for row_i in range(10)
+        ]
+        for sim_series, filename_template in zip(
+            (
+                'ccost_10',
+                'ccost_30',
+                'diffusion_02',
+                'diffusion_04',
+            ),
+            (
+                'avigdor-utcepoch-*-sth-{sth}-cth-{cth}-ccost-10.json*.h5',
+                'avigdor-utcepoch-*-sth-{sth}-cth-{cth}-ccost-30.json*.h5',
+                'avigdor-utcepoch-*-sth-{sth}-cth-{cth}-ccost-36-D-0.2.json-*.h5',
+                'avigdor-utcepoch-*-sth-{sth}-cth-{cth}-ccost-36-D-0.4.json-*.h5',
+            )
+        )
+    }
 
-    data_dir = '../../strife-notebook/data/'
-    filename_templates = [
-        'avigdor-utcepoch-*-sth-{sth}-cth-{cth}-ccost-10.json*.h5',
-        'avigdor-utcepoch-*-sth-{sth}-cth-{cth}-ccost-30.json*.h5',
-        'avigdor-utcepoch-*-sth-{sth}-cth-{cth}-ccost-36-D-0.2.json-*.h5',
-        'avigdor-utcepoch-*-sth-{sth}-cth-{cth}-ccost-36-D-0.4.json-*.h5',
-    ]
-    [
-        filename_template_ccost_10,
-        filename_template_ccost_30,
-        filename_template_diffusion_02,
-        filename_template_diffusion_04,
-    ] = [data_dir + n for n in filename_templates]
 
-    return dict(
-        ccost_10 = open_h5py_files(get_filename_list(filename_template_ccost_10)),
-        ccost_30 = open_h5py_files(get_filename_list(filename_template_ccost_30)),
-        diffusion_02 = open_h5py_files(get_filename_list(filename_template_diffusion_02)),
-        diffusion_04 = open_h5py_files(get_filename_list(filename_template_diffusion_04)),
-    )
-
-def beaut(fig, axes):
+#@contract(fig=mpl.figure.Figure, axes='list[10](list[10])')
+def beautify(fig, axes):
     for row_i, axes_row in enumerate(axes):
         for col_i, ax in enumerate(axes_row):
             ax.set_xticks([])
@@ -130,6 +138,7 @@ def beaut(fig, axes):
 
     fig.tight_layout()
 
+
 def make_plots(data, plotter):
     fig, axes = plt.subplots(
         figsize=(13,13),
@@ -141,7 +150,7 @@ def make_plots(data, plotter):
         for col_i in range(10):
                 plotter(axes, data, row_i, col_i)
 
-    beaut(fig, axes)
+    beautify(fig, axes)
 
     return fig, axes
 
@@ -165,16 +174,19 @@ def freqs_plotter(axes, freqs_data, sth, cth):
             color=strain_attr['color']
         )
 
+
 def make_freqs_plots(ff_list_list):
     data = get_simulation_freqs_data_grid(ff_list_list)
     fig, axes = make_plots(data, freqs_plotter)
 
     return fig, axes
 
+
 def animate_func(ff_list_list, im_list_list, snapshot_i):
     for im_list, ff_list in zip(im_list_list, ff_list_list):
         for im, ff in zip(im_list, ff_list):
             im.set_data(ff[HDF5_SNAPSHOTS_DATA_PATH][snapshot_i])
+
 
 def make_pngs(ff_list_list, fname):
     # make a color map of fixed colors
@@ -201,12 +213,13 @@ def make_pngs(ff_list_list, fname):
         for ff, ax in zip(ff_row, axes_row)]
         for ff_row, axes_row in zip(ff_list_list, axes)]
 
-    beaut(fig, axes)
+    beautify(fig, axes)
 
     for snapshot_i in range(ff_list_list[0][0][HDF5_SNAPSHOTS_DATA_PATH].shape[0]):
         animate_func(ff_list_list, im_list_list, snapshot_i)
         temp_fname = '_tmp{:03d}'.format(snapshot_i)+fname+'.png'
         fig.savefig(temp_fname)
+
 
 def make_video(fname):
     print('Making movie animation.mpg - this make take a while')
@@ -221,50 +234,76 @@ def make_video(fname):
 
 
 #@contract(a_board='array[NxN](int32),N>0', returns='dict[4](int: array[NxN](int32),N>0)')
-@jit('int32[:,:](int32[:,:],int32[:,:])', nopython=True)
-def count_neighbors_of_strains(a_board, res):
-    row_num, col_num = a_board.shape
-    for central_strain in range(4,8):
-        for neighbor_strain in range(4,8):
-            for central_row_i in range(row_num):
-                for central_col_i in range(col_num):
-                    for delta_row_i in range(-1, 2):
-                        for delta_col_i in range(-1, 2):
-                            neighbor_row_i = central_row_i + delta_row_i
-                            neighbor_col_i = central_col_i + delta_col_i
-                            if neighbor_row_i >= row_num:
-                                neighbor_row_i = neighbor_row_i - row_num
-                            if neighbor_col_i >= col_num:
-                                neighbor_col_i = neighbor_col_i - col_num
-                            is_central_strain = central_strain == a_board[central_row_i, central_col_i]
-                            if is_central_strain:
-                                is_neighbor_strain = neighbor_strain == a_board[neighbor_row_i, neighbor_col_i]
-                                if is_neighbor_strain:
-                                    res[central_strain, neighbor_strain] += 1
+
+@jit('void(int32[:,:,:], int32[:,:,:])', nopython=True)
+def jitted_count_neighbors(a_board, res_board):
+    snapshot_num, row_num, col_num = a_board.shape
+    for snapshot_i in range(snapshot_num):
+        for central_strain in range(4,8):
+            for neighbor_strain in range(4,8):
+                for central_row_i in range(row_num):
+                    for central_col_i in range(col_num):
+                        for delta_row_i in range(-1, 2):
+                            for delta_col_i in range(-1, 2):
+                                neighbor_row_i = (central_row_i + delta_row_i) % row_num
+                                neighbor_col_i = (central_col_i + delta_col_i) % col_num
+
+                                is_central_strain = central_strain == a_board[snapshot_i, central_row_i, central_col_i]
+                                is_neighbor_strain = neighbor_strain == a_board[snapshot_i, neighbor_row_i, neighbor_col_i]
+
+                                if is_central_strain and is_neighbor_strain:
+                                    res_board[snapshot_i, central_strain, neighbor_strain] = res_board[snapshot_i, central_strain, neighbor_strain] + 1
+
+
+@jit('int32[:,:,:](int32[:,:,:])')
+def count_neighbors(a_board):
+    """Return count of neighbors of each strain for each strain of central cell
+
+    For each board snapshot `snapshot_i` and for each cell of strain
+    `central_strain`, count the total number of cells of each cell of strain
+    `neighbor_strain` which are inside the Moore neighborhood around cells of
+    strain `central_strain`.
+    """
+    res_shape = a_board.shape[0], 8, 8
+    res_board = np.zeros(res_shape, dtype='int32')
+    jitted_count_neighbors(a_board, res_board)
+    return res_board
+
+
+def count_total_strains(a_board):
+    res = np.sum(a_board == strain_enum, axis=(1,2))
+    for strain_enum in range(4, 8):
+        res = np.vstack(res, sum(a_board == strain_enum), axis=(1,2))
     return res
 
 
-def test_neigbor_count():
+
+def count_relative_neighbors(a_board):
+    neighbors_count = count_neighbors(a_board)
+    total_count_total_strains(a_board)
+
+
+def test_neighbors_count():
     test = h5py.File(
         # data filename
-        '../../../strife-data/data/avigdor-sth-5-cth-5-ccost-10.json-1361510454900658000-2000-603157824.h5'
+        'data/avigdor-sth-5-cth-5-ccost-10.json-1361510454900658000-2000-603157824.h5'
     )[
         # hdf5 data set path name
         '/DataSamples/Snapshots/Data'
-    ] # sample #150
+    ][...]
 
-    #print(test[5:10,5:10])
-    for i in range(1):
-        print(i)
-        a = test[i][...]
-        res = np.zeros((8,8), dtype='int32')
-        count_neighbors_of_strains(a, res)
-        print(res)
-        print(i)
+    #total_strain_count_board = count_strains()
+
+    neighbors_count = count_neighbors(test)
+    print(neighbors_count)
+
+
+def test():
+    test_neighbors_count()
 
 
 def main():
-    print(count_neighbors_of_strains.inspect_types())
+    test()
     #ff_dict = get_hdf5_grid()
 
     #print('make freqs plots')
@@ -276,7 +315,6 @@ def main():
     #    make_pngs(ff_dict[data_set_name], data_set_name)
     #    make_video(data_set_name)
 
-    test_neigbor_count()
 
 
 if __name__ == '__main__':
